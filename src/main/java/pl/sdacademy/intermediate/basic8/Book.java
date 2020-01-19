17b67512379f91a1d58857dc1b34b8462563a57c
Grzegorz Wdowczyk <gw@wp.pl>
2020-01-19 13:55:13 +0100

    Testy

---
+++ src/main/java/pl/sdacademy/intermediate/basic8/Book.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.intermediate.basic8;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @author Piotr Zietek
+ */
+@Builder
+@ToString
+@Getter
+class Book {
+
+    private String title;
+    private String author;
+    private int yearPushlished;
+    private int numberOfPages;
+    private double price;
+    private Genre genre;
+}
---
+++ src/main/java/pl/sdacademy/intermediate/basic8/Genre.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.intermediate.basic8;
+
+/**
+ * @author Piotr Zietek
+ */
+enum Genre {
+
+    FOR_CHILDREN,
+    CRIME_STORY,
+    FANTASY
+}
